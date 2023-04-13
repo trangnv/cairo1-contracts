b4332c490fa6291cc96239758fdf257842e895e8
trangnv <nvtrang91@gmail.com>
2023-04-13 23:08:15 +0700

    obtaining .json and .casm files with subprocess

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Smart contracts with Cairo1
 
-Collections of smart contracts written in Cairo1, each contract lives in a separate git branch. The general workflow: Scarb for package management and compilation; starknet.py for declaration and deployment.
+Collections of smart contracts written in Cairo1 The general workflow: Scarb for package management and compilation; starknet.py for declaration and deployment.
 
-- [Hello Starknet]()
+- Hello Starknet
--- scripts/deploy.py
+++ scripts/deploy.py
@@ -2,13 +2,14 @@ from starknet_py.common import create_casm_class
 from starknet_py.hash.casm_class_hash import compute_casm_class_hash
 from accounts import get_account
 import asyncio
+import subprocess
 
 account = get_account()
 MAX_FEE=int(1e16)
 
-async def declare_contract(contract_compiled_casm_file, compiled_contract_file):
+async def declare_contract(contract_compiled_casm, compiled_contract):
     # contract_compiled_casm is the output of the starknet-sierra-compile (.casm file)
-    casm_class = create_casm_class(contract_compiled_casm_file)
+    casm_class = create_casm_class(contract_compiled_casm)
 
     # Compute Casm class hash
     casm_class_hash = compute_casm_class_hash(casm_class)
@@ -16,7 +17,7 @@ async def declare_contract(contract_compiled_casm_file, compiled_contract_file):
     # Create Declare v2 transaction
     declare_v2_transaction = await account.sign_declare_v2_transaction(
         # compiled_contract is the output of the starknet-compile (.json file)
-        compiled_contract=compiled_contract_file,
+        compiled_contract=compiled_contract,
         compiled_class_hash=casm_class_hash,
         max_fee=MAX_FEE,
     )
@@ -26,9 +27,18 @@ async def declare_contract(contract_compiled_casm_file, compiled_contract_file):
     await account.client.wait_for_tx(resp.transaction_hash)
 
     sierra_class_hash = resp.class_hash
+    print(sierra_class_hash)
 
 def main():
-    print(hex(account.address))
+    # starknet-compile and save to json file
+    subprocess.call(['starknet-compile', 'src/hello_starknet.cairo', 'src/hello_starknet.json'])
+    ## read the json file
+    with open('src/hello_starknet.json', 'r') as f:
+        compiled_contract = f.read()
+
+    # starknet-sierra-compile, dont need to save 
+    contract_compiled_casm = subprocess.getstatusoutput('starknet-sierra-compile src/hello_starknet.json --add-pythonic-hints')[1]
+    asyncio.run(declare_contract(contract_compiled_casm, compiled_contract))
 
 if __name__ == "__main__":
     main()
